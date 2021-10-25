9e9805f27e93c3f7338cf1f045531f63c1080b5f
Fabricio Calvanese Augusto <fabriciocalvanese@gmail.com>
2021-10-25 12:11:52 -0300

    ajustes na conversão de tipos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,11 +6,17 @@ fun main(args: Array<String>) {
     var area = 0.0
     var perimetro = 0.0
 
-    println("Digite o diâmetro do circulo: ")
+    print("Digite o diâmetro do circulo: ")
     diametro = readLine()!!.toInt()
 
-    raio = diametro / 2
+    raio = diametro/ 2
+    raio.toDouble()
 
     area = pi * (raio * raio)
+    area.toDouble()
+
     perimetro = 2 * pi * raio
+    perimetro.toDouble()
+
+    println("A área do circulo é: $area, e o perimetro do circulo: $perimetro")
 }
